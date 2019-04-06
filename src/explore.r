5d289ddb6f0df527104bc634796229ed4f625a1b
Andrew Welsh <andrew-welsh626@gmail.com>
2019-04-06 11:25:07 -0400

    joined by playerId

--- src/explore.r
+++ src/explore.r
@@ -1,7 +1,11 @@
+library(lubridate)
+library(tidyverse)
+
+games <- read.csv("data/games.csv")
 wellness <- read.csv("data/wellness.csv")
 gps <- read.csv("data/gps.csv")
 rpe <- read.csv("data/rpe.csv")
 
 wellnessSubset <- wellness[sample(nrow(wellness), 200),]
-gpsSubset <- gps[sample(nrow(gps), 200),]
-rpeSubset <- rpe[sample(nrow(rpe), 200),]
+gpsSubset <- left_join(gps, wellnessSubset)
+rpeSubset <- left_join(rpe, gpsSubset)
